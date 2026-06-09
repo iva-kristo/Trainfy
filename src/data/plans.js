38d5912a8310c659d6a65e63a0e687f473d6932d
Iva Kristo <iva.kristo03@gmail.com>
2026-06-09 21:11:31 +0200

    Add training plans data

---
+++ src/data/plans.js
@@ -0,0 +1,35 @@
+export const planovi = [
+  {
+    id: "mrsavljenje",
+    naziv: "Plan za mršavljenje",
+    cilj: "Mršavljenje",
+    vjezbe: [
+      { naziv: "Brzo hodanje na traci", ponavljanja: 10, serije: 3, tezina: 0 },
+      { naziv: "Čučnjevi", ponavljanja: 15, serije: 3, tezina: 0 },
+      { naziv: "Iskoraci", ponavljanja: 12, serije: 3, tezina: 0 },
+      { naziv: "Plank", ponavljanja: 45, serije: 3, tezina: 0 },
+    ],
+  },
+  {
+    id: "izgradnja-misica",
+    naziv: "Plan za izgradnju mišića",
+    cilj: "Izgradnja mišića",
+    vjezbe: [
+      { naziv: "Potisak s klupe", ponavljanja: 10, serije: 4, tezina: 60 },
+      { naziv: "Mrtvo dizanje", ponavljanja: 8, serije: 4, tezina: 80 },
+      { naziv: "Čučanj sa šipkom", ponavljanja: 10, serije: 4, tezina: 70 },
+      { naziv: "Zgibovi", ponavljanja: 8, serije: 3, tezina: 10 },
+    ],
+  },
+  {
+    id: "kondicija",
+    naziv: "Plan za kondiciju",
+    cilj: "Kondicija",
+    vjezbe: [
+      { naziv: "Trčanje", ponavljanja: 5, serije: 4, tezina: 0 },
+      { naziv: "Burpee", ponavljanja: 15, serije: 4, tezina: 0 },
+      { naziv: "Vijača", ponavljanja: 50, serije: 4, tezina: 0 },
+      { naziv: "Trbušnjaci", ponavljanja: 20, serije: 3, tezina: 0 },
+    ],
+  },
+];
